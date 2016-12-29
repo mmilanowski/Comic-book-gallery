bcee5c10a1c5eb84d6fa36c11a60484372f91a89
mattm <mmilanowski@yahoo.com>
2016-12-29 11:42:24 -0600

    first content return

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+                
+            }
+            return Content("Hello form Comic book controller");
+
+
+        
+            //return "Hello form Comic book controller";
+        }
+
+    }
+}
